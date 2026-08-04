d27d38b4ad4935606fc44251f6403ccebc1a1eb7
Paul112-git <p74323699@gmail.com>
2026-08-04 13:41:52 +0100

    Update move.py

--- move.py
+++ move.py
@@ -1,157 +1,164 @@
-import os 
-import time
+import os
 import shutil
+import time
 while True:
     try:
-        folder = input("Enter a file path: ").lower()
-        if folder == "":
-            print ("Please type in your file path")
-            exit()
-        else:
-            files = os.listdir(folder)
-            photo = 0
-            music = 0
-            videos = 0
-            Documents = 0
-            Archives = 0
-            Di = 0
-            programs = 0
-            ap = 0
-            pp = 0
-            Excel = 0
-            wf = 0
-            pf = 0  
-            skipped = 0
-            for file in files:
-                sources = os.path.join(folder, file)
-    #----musics----
-                if file.endswith((".mp3", ".m4a", ".wav", ".acc", "flac", ".ogg", ".wma")):
-                    if not os.path.exists(os.path.join(folder, "Music")):
-                         os.mkdir(os.path.join(folder, "Music"))
-                    shutil.move(sources, os.path.join(folder, "Music"))
-                    music +=1
-    #----videos-----
-                elif file.endswith((".mp4", ".mkv", ".lrc", ".mov", ".wmv", ".flv", ".webm", ".mpeg", ".3gp", ".pls")):
-                    if not os.path.exists(os.path.join(folder, "Videos")):
-                        os.mkdir(os.path.join(folder, "Videos"))
-                    shutil.move(sources, os.path.join(folder, "Videos"))
-                    videos +=1
-    
-        #----photos----
-                elif file.endswith((".png", ".jpeg", ".jpg", ".gif", ".bmp", ".webp", ".svg", ".tiff", ".ico")):
-                    if not os.path.exists(os.path.join(folder, "Photos")):
-                        os.mkdir(os.path.join(folder, "Photos"))
-                    shutil.move(sources, os.path.join(folder, "Photos"))
-                    photo +=1
+        folder = input("Enter a File Path (e  to exit): ").lower()
+        if folder == "e":
+            print("Bye")
+            break
+        files = os.listdir(folder)
+        music = 0
+        videos = 0
+        doc = 0
+        programs = 0
+        apk = 0
+        excel = 0
+        pyfile = 0
+        webfiles = 0
+        non_ext = 0
+        photos =0
 
-        #-----archives------
-                elif file.endswith(( ".zip", ".rar", ".7z", ".tar", ".gz")):
-                    if not os.path.exists(os.path.join(folder, "Archives")):
-                        os.mkdir(os.path.join(folder, "Archives"))
-                    shutil.move(sources, os.path.join(folder, "Archives"))
-                    Archives +=1
-
-    #----Documents-----
-                elif file.endswith(( ".pdf", ".doc", ".docx", ".txt", ".rtf", ".odt")):
-                    if not os.path.exists(os.path.join(folder, "Documents")):
-                        os.mkdir(os.path.join(folder, "Documents"))
-                    shutil.move(sources, os.path.join(folder, "Documents"))
-                    Documents +=1
-    
-    #----Excel-----
-                elif file.endswith((".csv", ".slsx", ".xls")):
-                    if not os.path.exists(os.path.join(folder, "Excel")):
-                        os.mkdir(os.path.join(folder, "Excel"))
-                    shutil.move(sources, os.path.join(folder, "Excel"))
-                    Excel +=1
-    #----power_point----
-                elif file.endswith(("pptx", "ppt")):
-                    if not os.path.exists(os.path.join(folder, "Power_point")):
-                        os.mkdir(os.path.join(folder, "Power_point"))
-                    shutil.move(sources, os.path.join(folder, "Power_point"))
-                    pp +=1
-    #----python_files----
-                elif file.endswith(( ".pyw", ".py")):
-                    if not os.path.exists(os.path.join(folder, "Python_files")):
-                        os.mkdir(os.path.join(folder, "Python_files"))
-                    shutil.move(sources, os.path.join(folder, "Python_files"))
-                    pf +=1
-    #----android----
-                elif file.endswith(( ".apk")):
-                    if not os.path.exists(os.path.join(folder, "Android_apps")):
-                        os.mkdir(os.path.join(folder, "Android_apps"))
-                    shutil.move(sources, os.path.join(folder, "Android_apps"))
-                    ap +=1
-    #----disk images----
-                elif file.endswith(( ".iso")):
-                    if not os.path.exists(os.path.join(folder, "Disk_images")):
-                        os.mkdir(os.path.join(folder, "Disk_images"))
-                    shutil.move(sources, os.path.join(folder, "Disk_images"))
-                    Di +=1
-    #-----web files-----
-                elif file.endswith(( ".html", ".css", ".js", ".json", ".xml")):
-                    if not os.path.exists(os.path.join(folder, "web_file")):
-                        os.mkdir(os.path.join(folder, "web_file"))
-                    shutil.move(sources, os.path.join(folder, "web_file"))
-                    wf +=1
-    #-----programs-----
-                elif file.endswith((".exe", ".msl")):
-                    if not os.path.exists(os.path.join(folder, "Programs")):
-                        os.mkdir(os.path.join(folder, "Programs"))
-                    shutil.move(sources, os.path.join(folder, "Programs"))
-                    programs+=1
-    except FileNotFoundError:
-        print(f"'{folder}' Not Found!")
-        exit()
+        counters={
+            "music": 1,
+            "videos": 1,
+            "photo": 1,
+            "doc": 1,
+            "programs":1,
+            "pyfile": 1,
+            "webfile": 1,
+            "apk": 1,
+            "excel": 1,
+            "Non-ext": 1
+        }
+        for file in files:
+            source = os.path.join(folder, file)
+            if os.path.isdir(source):
+                continue
+            name, ext = os.path.splitext(file)
+            #Videos
+            if file.endswith((".mp4", ".mkv", ".lrc", ".mov", ".wmv", ".flv", ".webm", ".mpeg", ".3gp", ".pls")):
+                if not os.path.exists(os.path.join(folder, "Videos")):
+                    os.mkdir(os.path.join(folder, "Videos"))
+                new = f"Videos-{counters["videos"]}-{ext}"
+                shutil.move(source, os.path.join(folder, "Videos", new))
+                videos+=1
+                counters["videos"]+=1
+            #music
+            elif file.endswith((".mp3", ".m4a", ".wav", ".acc", "flac", ".ogg", ".wma")):
+                if not os.path.exists(os.path.join(folder, "Music")):
+                    os.mkdir(os.path.join(folder, "Music"))
+                new = f"Music-{counters['music']}-{ext}"
+                shutil.move(source, os.path.join(folder, "Music"))
+                music+=1
+                counters["music"]+=1
+            #Photo
+            elif file.endswith((".png", ".jpeg", ".jpg", ".gif", ".bmp", ".webp", ".svg", ".tiff", ".ico")):
+                if not os.path.exists(os.path.join(folder, "Photos")):
+                    os.mkdir(os.path.join(folder, "Photos"))
+                new = f"Photos-{counters['photo']}-{ext}"
+                shutil.move(source, os.path.join(folder, "Photos", new))
+                photos +=1
+                counters["photo"]+=1
+            #Pyfiles
+            elif file.endswith((".png", ".jpeg", ".jpg", ".gif", ".bmp", ".webp", ".svg", ".tiff", ".ico")):
+                if not os.path.exists(os.path.join(folder, "Projects.py")):
+                    os.mkdir(os.path.join(folder, "Projects.py"))
+                new = f"Py_files-{counters['pyfiles']}-{ext}"
+                shutil.move(source, os.path.join(folder, "Projects.py", new))
+                pyfile +=1
+                counters["pyfile"]+=1
+            #Documents
+            elif file.endswith((".pdf", ".doc", ".docx", ".txt", ".rtf", ".odt")):
+                if not os.path.exists(os.path.join(folder, "Documents")):
+                    os.mkdir(os.path.join(folder, "Documents"))
+                new = f"Documents-{counters['doc']}-{ext}"
+                shutil.move(source, os.path.join(folder, "Documents", new))
+                doc +=1
+                counters[doc]+=1
+            #Excel
+            elif file.endswith((".xls", "csv", "xlsx")):
+                if not os.path.exists(os.path.join(folder, "Excel")):
+                    os.mkdir(os.path.join(folder, "Excel"))
+                new = f"Excel-{counters['excel']}-{ext}"
+                shutil.move(source, os.path.join(folder, "Excel", new))
+                excel +=1
+                counters['excel']+=1
+            #apk
+            elif file.endswith(".apk"):
+                if not os.path.exists(os.path.join(folder, "Apks")):
+                    os.mkdir(folder, "Apks")
+                new = f"Apks-{counters['doc']}-{ext}"
+                shutil.move(source, os.path.join(folder, "Documents", new))
+                apk +=1
+                counters["apk"]+=1
+            #programs
+            elif file.endswith((".exe", "msl")):
+                if not os.path.exists(os.path.join(folder, "Programs")):
+                    os.mkdir(os.path.join(folder, "Programs"))
+                new = f"Programs-{counters['doc']}-{ext}"
+                shutil.move(source, os.path.join(folder, "Programs", new))
+                programs +=1
+                counters['programs']+=1
+            #Webfiles
+            elif file.endswith((".html", ".css", ".js", ".json", ".xml")):
+                        if not os.path.exists(os.path.join(folder, "Web-files")):
+                            os.mkdir(folder, "Web-files")
+                        new = f"{counters['webfile']}-{ext}"
+                        shutil.move(source, os.path.join(folder, "Web-files", new))
+                        webfiles+=1
+                        counters["webfile"]+=1
+            else:
+                if ext == "":
+                    if not os.path.exists(os.path.join(folder, "Non-extension")):
+                        os.mkdir(os.path.join(folder, "Non-extension"))
+                    new = f"Non-extension{counters["Non-ext"]}"
+                    shutil.move(source, os.path.join(folder, "Non-extension", new))
+                    counters["Non-ext"]+=1
+                    non_ext +=1
+    except FileExistsError:
+        print (f"{file} Already Found")
     except shutil.Error:
-        print (f"{file} Already exists in  a folder Skipping.....")
-        time.sleep(1)
-        skipped +=1
-       
-    total = photo + music + videos + Archives + ap + Di + programs + Documents + pf + pp + wf + Excel
-    skip = skipped
-    print (f"Photos: ({photo})")
-    time.sleep(0.1) 
-    print (f"Videos: ({videos})")
-    time.sleep(0.1)  
-    print (f"Musics: ({music})")
-    time.sleep(0.1) 
-    print (f"Archives: ({Archives})")
-    time.sleep(0.1) 
-    print (f"Photos: ({photo})")
-    time.sleep(0.1) 
-    print (f"Excel: ({Excel})")
-    time.sleep(0.1)
-    print (f"Python_Files: ({pf})")
-    time.sleep(0.1) 
-    print (f"Androids: ({ap})")
-    time.sleep(0.1) 
-    print (f"Programs: ({programs})")
-    time.sleep(0.1) 
-    print (f"Web_Files: ({wf})")
-    time.sleep(0.1) 
-    print (f"Dcouments: ({Documents})")
-    time.sleep(0.1)  
-    print (f"Power_Point: ({pp})")
-    time.sleep(0.1) 
-    print (f"Disk_images: ({Di})")
-    time.sleep(0.1)
-    print (f"Skipped:({skipped})")
-    time.sleep(1) 
-    print ("Total Moved:", (total) )
-    print (f"Total Skipped: ({skip})")
-    print (skip, "File Skipped")
-    if total == 0:
-        print (f"No File Was Found In '({folder})'")
-    print (total, f"File organized! \nLocation: ({folder})\n")
-
-    ask = input("Organize Another File (y/n): ").lower()
-
-    if ask == "y":
+        print (f"{file} already exists skipped")
+    except FileNotFoundError:
+        print(f"Cannot Locate {folder}. check if the folder was type in correctly")
         continue
 
-    elif ask == "n":
-         print ("Bye!")
-         break
-    else:
-        print ("invalid input")
+    total = programs + excel + pyfile + apk + music + videos + webfiles + doc + non_ext + photos
+    print(f"apk >>> {apk}")
+    print("-_" * 24)
+    time.sleep(0.4)
+    print(f"Excel >>> {excel}")
+    print("-_" * 24)
+    time.sleep(0.4)
+    print(f"Photos >>> {photos}")
+    print("-_" * 24)
+    time.sleep(0.4)
+    print(f"Programs >>> {programs}")
+    print("-_" * 24)
+    time.sleep(0.4)
+    print(f"Webfile >>> {webfiles}")
+    print("-_" * 24)
+    time.sleep(0.4)
+    print(f"Pyfiles >>> {pyfile}")
+    print("-_" * 24)
+    time.sleep(0.4)
+    print(f"Musics >>> {music}")
+    print("-_" * 24)
+    time.sleep(0.4)
+    print(f"Videos >>> {videos}")
+    print("-_" * 24)
+    time.sleep(0.4)
+    print(f"Documents >>> {doc}")
+    print("-_" * 24)
+    time.sleep(0.4)
+    print(f"Files with Non-extension >>> {non_ext}")
+    print("-_" * 24)
+    time.sleep(2)
+    print("\n")
+    print("-_" * 24)
+    print (f"Total: {total} Files Moved")
+    if total == 0:
+        print (f"No file was Moved in '{folder}'. check if the folder contains any files")
+    
+
